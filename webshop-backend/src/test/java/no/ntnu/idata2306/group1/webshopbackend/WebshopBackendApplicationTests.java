dff4fbc276e4c00ebe415d7f260f71688bd6ff09
Julian Montague <juliangm@stud.ntnu.no>
2023-01-15 22:11:16 +0100

    Initial commit: backend Spring Boot project
    Generated with [Spring Initializr][1].
    
    [1]: https://start.spring.io

---
+++ webshop-backend/src/test/java/no/ntnu/idata2306/group1/webshopbackend/WebshopBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.idata2306.group1.webshopbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebshopBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
