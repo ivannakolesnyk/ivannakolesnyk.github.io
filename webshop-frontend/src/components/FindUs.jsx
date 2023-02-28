e9858efcf7c962aefb8e72eaaca8121c77e224a0
Ivanka Kolesnyk <ivankakolesnik17@gmail.com>
2023-02-28 20:02:32 +0100

    Implement google maps without marker

--- webshop-frontend/src/components/FindUs.jsx
+++ webshop-frontend/src/components/FindUs.jsx
@@ -1,16 +1,32 @@
 import React from "react";
-import { Typography } from "@mui/material";
+import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
+
+const libraries = ["places"];
+const mapContainerStyle = {
+  width: "100%",
+  height: "600px",
+};
 
 const FindUs = () => {
+  const { isLoaded, loadError } = useLoadScript({
+    googleMapsApiKey: "AIzaSyDPk_iIoIf7gKWMTEdQ6G5OzlwrmxJ-TpY",
+    libraries,
+  });
+
+  if (loadError) return "Error loading maps";
+  if (!isLoaded) return "Loading maps";
+
   return (
-    <>
-      <Typography variant="h3" align="left">
-        Find us
-      </Typography>
-      <Typography variant="body1">
-        Ivanka's fancy map function will be implemented here.
-      </Typography>
-    </>
+    <GoogleMap
+      mapContainerStyle={mapContainerStyle}
+      center={{
+        lat: 62.47194,
+        lng: 6.23559,
+      }}
+      zoom={15}
+    >
+      <Marker position={"Larsgårdsvegen 2, 6009 Ålesund"} />
+    </GoogleMap>
   );
 };
 
