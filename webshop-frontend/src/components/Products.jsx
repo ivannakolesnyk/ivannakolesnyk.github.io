9d0237e34f00e8767015c492ccac0226d8e464bc
Mark Kour <mark.kour@icloud.com>
2023-03-02 08:40:42 +0100

    Tested the ProductCard component with fake data

--- webshop-frontend/src/components/Products.jsx
+++ webshop-frontend/src/components/Products.jsx
@@ -2,17 +2,99 @@ import React from "react";
 
 import { Box, Stack } from "@mui/system";
 import Categories from "./Categories";
+import ProductCard from "./ProductCard";
 
 const Products = () => {
+  // Fetching product data from API
+  const products = [
+    {
+      id: 1,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+    {
+      id: 2,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+    {
+      id: 3,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+    {
+      id: 4,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+    {
+      id: 5,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+    {
+      id: 6,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+    {
+      id: 7,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+    {
+      id: 8,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+    {
+      id: 9,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+    {
+      id: 10,
+      productName: "Coffee",
+      price: 67,
+      imagePath:
+        "http://cdn.shopify.com/s/files/1/0548/9469/0401/products/WHOLE_2.png?v=1668467257",
+    },
+  ];
+
   return (
     <Box>
-      <Stack direction="row" spacing={1} justifyContent="space-between">
+      <Stack direction="row" justifyContent="space-between">
         <Box flex={1} p={4}>
           <Categories />
         </Box>
-        <Box bgcolor={"blue"} flex={5} p={4}>
-          Products
-        </Box>
+        <Stack direction="row" gap={"50px"} flex={5} p={4} flexWrap="wrap">
+          {products.map(({ id, productName, price, imagePath }) => (
+            <ProductCard
+              key={id}
+              productName={productName}
+              price={price}
+              imagePath={imagePath}
+            />
+          ))}
+        </Stack>
       </Stack>
     </Box>
   );
