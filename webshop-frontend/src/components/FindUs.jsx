836ae713ac7e47e3a3558724ba1d7c408a997a3f
Ivanka Kolesnyk <ivankakolesnik17@gmail.com>
2023-03-04 21:14:58 +0100

    Added comments

--- webshop-frontend/src/components/FindUs.jsx
+++ webshop-frontend/src/components/FindUs.jsx
@@ -5,6 +5,7 @@ import { theme } from "../theme";
 import { GoogleMap, Marker, useLoadScript } from "@react-google-maps/api";
 import DirectionsIcon from "@mui/icons-material/Directions";
 
+//For placing Direction Butoon component right on top of the Google Map, used wraping both the GoogleMap and DirectionButton components inside a container element and use CSS to position the last one over the map.
 const DirectionButton = ({ address, onGetDirections }) => {
   return (
     <ThemeProvider theme={theme}>
@@ -25,6 +26,7 @@ const DirectionButton = ({ address, onGetDirections }) => {
           zIndex: 1,
           "& > *": {
             margin: "10px 0",
+            // last two lines used for spacing between text
           },
         }}
       >
@@ -51,6 +53,7 @@ const DirectionButton = ({ address, onGetDirections }) => {
   );
 };
 
+//Function for implementation the Google maps to the page
 const FindUs = () => {
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: "AIzaSyDPk_iIoIf7gKWMTEdQ6G5OzlwrmxJ-TpY",
@@ -60,6 +63,9 @@ const FindUs = () => {
   const mapRef = useRef(null);
   const onMapLoad = useCallback((map) => {
     mapRef.current = map;
+    // Used the useRef hook for getting a reference to the Google Map object. When the onLoad event of the GoogleMap component is triggered, the onMapLoad function is called, which updates the mapRef value with the reference to the Google Map object. This reference is used later in the component to interact with the map, such as adding markers, controlling zoom levels, etc.
+
+    //The useCallback hook is used to memoize the onMapLoad function so that it doesn't change on every render. If the onMapLoad function was not memoized with useCallback, it would be recreated on every render, and as a result, the GoogleMap component would think that it needs to be reloaded every time the function changes. This would cause unnecessary re-renders and could lead to performance issues. By using useCallback, the onMapLoad function is only created once and memoized for future use, which helps to avoid unnecessary re-renders.
   }, []);
 
   if (loadError) return "Error loading maps";
@@ -80,7 +86,7 @@ const FindUs = () => {
         onGetDirections={() => {
           const url = `https://www.google.com/maps/dir/?api=1&destination=Larsgårdsvegen+2,+6009+Ålesund`;
           window.open(url, "_blank");
-        }}
+        }} // Open a new tab with your path in Google Maps after click on the button
       />
     </div>
   );
