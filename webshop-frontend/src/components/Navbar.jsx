d3531ab00342086a8b0c33be92c0af17e03caaad
Simon Gjøen <s_gjoen@hotmail.com>
2023-03-09 18:02:07 +0100

    Clean up Navbar and add "html {overflow-y: scroll;}"
    The Navbar had some "early learning mistakes" which are now hopefully all removed. "html {overflow-y: scroll;}" prevents the content from moving when a page needs a scroll bar.

--- webshop-frontend/src/components/Navbar.jsx
+++ webshop-frontend/src/components/Navbar.jsx
@@ -43,12 +43,6 @@ appealing and responsive to different screen sizes.
     justifyContent: "space-between",
     backgroundColor: "white",
     color: theme.palette.primary.contrastText,
-    "& .MuiSvgIcon-root": {
-      color: theme.palette.primary.contrastText,
-    },
-    "& button": {
-      color: theme.palette.primary.contrastText,
-    },
   });
 
   /**
@@ -64,19 +58,6 @@ It is used to control the state of the hamburger menu.
   const [openHamburgerMenu, setOpenHamburgerMenu] = useState(false);
 
   /**
-The SmallScreenNavbar component is a styled div that contains the
-contents of the Navbar on small screens.
-It is used to display the Navbar's contents in a way that is visually
-appealing and responsive to small screens.
-*/
-  const SmallScreenNavbar = styled(Toolbar)({
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "space-between",
-    width: "100%",
-  });
-
-  /**
 The menu button used on small screens
 */
   const MenuButton = styled(IconButton)({});
@@ -90,268 +71,285 @@ NavBar, which is meant for computer screens and larger screens.
 */
   return (
     <AppBar position="sticky">
-      <StyledToolbar>
-        {smallScreenSize ? (
-          <SmallScreenNavbar>
-            <Link component={Link} to="/">
-              <img
-                src={smallCoffeeLogo}
-                alt="Monoca logo"
-                style={{
-                  width: "auto",
-                  height: "3rem",
-                  paddingTop: "1rem",
-                }}
-              />
-            </Link>
-            <MenuButton
-              edge="end"
-              className={"menu-button"}
-              color="#fff"
-              aria-label="menu"
-              onClick={(e) => setOpenHamburgerMenu(true)}
-            >
-              <MenuIcon
-                sx={{
-                  color: theme.palette.secondary.main,
-                  fontSize: "3rem",
-                }}
-              />
-            </MenuButton>
-            <Menu
-              style={{ position: "absolute", top: 30, right: 0 }}
-              id="positioned-hamburger-menu"
-              aria-labelledby="positioned-hamburger-menu"
-              anchorEl={MenuButton}
-              open={openHamburgerMenu}
-              onClose={(e) => setOpenHamburgerMenu(false)}
-              anchorOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              transformOrigin={{
-                vertical: "top",
-                horizontal: "right",
-              }}
-              getContentAnchorEl={null}
-            >
-              <MenuItem
-                key="home"
-                component={Link}
-                to="/"
-                onClick={(e) => setOpenHamburgerMenu(false)}
-              >
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  Home
-                </Typography>
-              </MenuItem>
-              <MenuItem
-                key="menu"
-                component={Link}
-                to="/menu"
-                onClick={(e) => setOpenHamburgerMenu(false)}
-              >
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  Menu
-                </Typography>
-              </MenuItem>
-              <MenuItem
-                key="products"
-                component={Link}
-                to="/products"
-                onClick={(e) => setOpenHamburgerMenu(false)}
-              >
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  Products
-                </Typography>
-              </MenuItem>
-              <MenuItem
-                key="about"
-                component={Link}
-                to="/about"
-                onClick={(e) => setOpenHamburgerMenu(false)}
-              >
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  About us
-                </Typography>
-              </MenuItem>
-              <MenuItem
-                key="findus"
-                component={Link}
-                to="/findus"
-                onClick={(e) => setOpenHamburgerMenu(false)}
-              >
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  Find us
-                </Typography>
-              </MenuItem>
-              <MenuItem
-                key="login"
-                component={Link}
-                to="/login"
-                onClick={(e) => setOpenHamburgerMenu(false)}
-              >
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  Log in
-                </Typography>
-              </MenuItem>
-              <MenuItem
-                key="shoppingcart"
-                component={Link}
-                to="/shoppingcart"
-                onClick={(e) => setOpenHamburgerMenu(false)}
-              >
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  Shopping cart
-                </Typography>
-              </MenuItem>
-            </Menu>
-          </SmallScreenNavbar>
-        ) : (
-          <>
-            <div
-              id="left-navbar-buttons"
+      {smallScreenSize ? (
+        <StyledToolbar>
+          <Link component={Link} to="/">
+            <img
+              src={smallCoffeeLogo}
+              alt="Monoca logo"
               style={{
-                display: "flex",
-                justifyContent: "left",
-                width: "33.33%",
+                width: "auto",
+                height: "3rem",
+                paddingTop: "1rem",
               }}
-            >
-              <Button component={Link} to="/menu">
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  Menu
-                </Typography>
-              </Button>
-              <Button component={Link} to="/products">
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  Products
-                </Typography>
-              </Button>
-              <Button color="inherit" component={Link} to="/about">
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  About us
-                </Typography>
-              </Button>
-            </div>
-            <div
-              id="coffeeLogo"
+            />
+          </Link>
+          <MenuButton
+            edge="end"
+            className={"menu-button"}
+            color="#fff"
+            aria-label="menu"
+            onClick={(e) => setOpenHamburgerMenu(true)}
+          >
+            <MenuIcon
+              sx={{
+                color: theme.palette.secondary.main,
+                fontSize: "3rem",
+              }}
+            />
+          </MenuButton>
+          <Menu
+            style={{ position: "absolute", top: 30, right: 0 }}
+            id="positioned-hamburger-menu"
+            aria-labelledby="positioned-hamburger-menu"
+            anchorEl={MenuButton}
+            open={openHamburgerMenu}
+            onClose={(e) => setOpenHamburgerMenu(false)}
+            anchorOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            transformOrigin={{
+              vertical: "top",
+              horizontal: "right",
+            }}
+            getContentAnchorEl={null}
+          >
+            <MenuItem
+              key="home"
               component={Link}
-              style={{
-                display: "flex",
-                justifyContent: "center",
-                width: "33.33%",
-              }}
+              to="/"
+              onClick={(e) => setOpenHamburgerMenu(false)}
             >
-              <Link to="/">
-                <img
-                  src={coffeeLogo}
-                  alt="Monoca logo"
-                  style={{
-                    width: "auto",
-                    height: "4.5rem",
-                    paddingTop: "0.5rem",
-                    alignContent: "left",
-                  }}
-                />
-              </Link>
-            </div>
-            <div
-              id="right-navbar-buttons"
-              style={{
-                display: "flex",
-                justifyContent: "right",
-                width: "33.33%",
-              }}
-            >
-              <Button
-                startIcon={<PlaceOutlinedIcon />}
-                component={Link}
-                to="/findus"
+              <Typography
+                variant="button"
                 sx={{
                   color: theme.palette.primary.contrastText,
                 }}
               >
-                <Typography
-                  variant="button"
-                  sx={{
-                    color: theme.palette.primary.contrastText,
-                  }}
-                >
-                  Find us
-                </Typography>
-              </Button>
-              <Button
-                startIcon={<Person2OutlinedIcon />}
-                color="inherit"
-                component={Link}
-                to="/login"
+                Home
+              </Typography>
+            </MenuItem>
+            <MenuItem
+              key="menu"
+              component={Link}
+              to="/menu"
+              onClick={(e) => setOpenHamburgerMenu(false)}
+            >
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
               >
-                <Typography
-                  variant="button"
+                Menu
+              </Typography>
+            </MenuItem>
+            <MenuItem
+              key="products"
+              component={Link}
+              to="/products"
+              onClick={(e) => setOpenHamburgerMenu(false)}
+            >
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                Products
+              </Typography>
+            </MenuItem>
+            <MenuItem
+              key="about"
+              component={Link}
+              to="/about"
+              onClick={(e) => setOpenHamburgerMenu(false)}
+            >
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                About us
+              </Typography>
+            </MenuItem>
+            <MenuItem
+              key="findus"
+              component={Link}
+              to="/findus"
+              onClick={(e) => setOpenHamburgerMenu(false)}
+            >
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                Find us
+              </Typography>
+            </MenuItem>
+            <MenuItem
+              key="login"
+              component={Link}
+              to="/login"
+              onClick={(e) => setOpenHamburgerMenu(false)}
+            >
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                Log in
+              </Typography>
+            </MenuItem>
+            <MenuItem
+              key="shoppingcart"
+              component={Link}
+              to="/shoppingcart"
+              onClick={(e) => setOpenHamburgerMenu(false)}
+            >
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                Shopping cart
+              </Typography>
+            </MenuItem>
+          </Menu>
+        </StyledToolbar>
+      ) : (
+        <StyledToolbar>
+          <div
+            id="left-navbar-buttons"
+            style={{
+              display: "flex",
+              justifyContent: "left",
+              width: "33.33%",
+            }}
+          >
+            <Button component={Link} to="/menu">
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                Menu
+              </Typography>
+            </Button>
+            <Button component={Link} to="/products">
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                Products
+              </Typography>
+            </Button>
+            <Button color="inherit" component={Link} to="/about">
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                About us
+              </Typography>
+            </Button>
+          </div>
+          <div
+            id="coffeeLogo"
+            component={Link}
+            style={{
+              display: "flex",
+              justifyContent: "center",
+              width: "33.33%",
+            }}
+          >
+            <Link to="/">
+              <img
+                src={coffeeLogo}
+                alt="Monoca logo"
+                style={{
+                  width: "auto",
+                  height: "4.5rem",
+                  paddingTop: "0.5rem",
+                  alignContent: "left",
+                }}
+              />
+            </Link>
+          </div>
+          <div
+            id="right-navbar-buttons"
+            style={{
+              display: "flex",
+              justifyContent: "right",
+              width: "33.33%",
+            }}
+          >
+            <Button
+              startIcon={
+                <PlaceOutlinedIcon
                   sx={{
+                    fontSize: "2rem",
                     color: theme.palette.primary.contrastText,
                   }}
-                >
-                  Log in
-                </Typography>
-              </Button>
-              <IconButton component={Link} to="/shoppingcart" fontSize="small">
-                <ShoppingCartIcon sx={{ fontSize: "2rem" }} />
-              </IconButton>
-            </div>
-          </>
-        )}
-      </StyledToolbar>
+                />
+              }
+              component={Link}
+              to="/findus"
+              sx={{
+                color: theme.palette.primary.contrastText,
+              }}
+            >
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                Find us
+              </Typography>
+            </Button>
+            <Button
+              startIcon={
+                <Person2OutlinedIcon
+                  sx={{
+                    fontSize: "2rem",
+                    color: theme.palette.primary.contrastText,
+                  }}
+                />
+              }
+              color="inherit"
+              component={Link}
+              to="/login"
+            >
+              <Typography
+                variant="button"
+                sx={{
+                  color: theme.palette.primary.contrastText,
+                }}
+              >
+                Log in
+              </Typography>
+            </Button>
+            <IconButton component={Link} to="/shoppingcart" fontSize="small">
+              <ShoppingCartIcon
+                sx={{
+                  fontSize: "2rem",
+                  color: theme.palette.primary.contrastText,
+                }}
+              />
+            </IconButton>
+          </div>
+        </StyledToolbar>
+      )}
     </AppBar>
   );
 };
