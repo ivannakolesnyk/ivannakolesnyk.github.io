4931c6932feb937bd9bee53afe31997297b6447e
Mark Kour <mark.kour@icloud.com>
2023-03-11 15:38:28 +0100

    Change the variant of info box

--- webshop-frontend/src/components/Products.jsx
+++ webshop-frontend/src/components/Products.jsx
@@ -1,13 +1,13 @@
 import React from "react";
-
-import { Box, Stack } from "@mui/system";
+import { Box } from "@mui/system";
 import Categories from "./Categories";
 import ProductCard from "./ProductCard";
-import { Alert, Grid } from "@mui/material";
-import { theme } from "../theme";
+import { Grid, Typography } from "@mui/material";
+import { useTheme } from "@emotion/react";
 
 const Products = () => {
   // Fetching product data from API
+  const theme = useTheme();
   const products = [
     {
       id: 1,
@@ -89,19 +89,20 @@ const Products = () => {
       <Grid item md={10} p={4}>
         <Box
           sx={{
-            backgroundColor: "#F1D2C8",
-            color: "#7A4647",
+            backgroundColor: "primary.main",
+            color: theme.palette.primary.contrastText,
             padding: "10px",
             borderRadius: "8px",
-            // width: "90%",
             textAlign: "center",
             justifySelf: "center",
             margin: "0 auto",
-            marginBottom: 4,
+            marginBottom: "0.8rem",
           }}
         >
-          Awaken your senses with every sip - Experience the perfect cup of
-          coffee with us!
+          <Typography variant="body1">
+            Awaken your senses with every sip - Experience the perfect cup of
+            coffee with us!
+          </Typography>
         </Box>
         <Grid container spacing={5}>
           {products.map(({ id, productName, price, imagePath }) => (
