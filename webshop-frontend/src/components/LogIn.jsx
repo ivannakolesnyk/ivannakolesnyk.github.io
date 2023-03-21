8a626e08166f95bd1dcbdf70fdb6b0148b09fb76
Simon Gjøen <s_gjoen@hotmail.com>
2023-03-21 11:20:16 +0100

    Add login logic to LogIn.jsx
    This is just example code that will be need to be edited later.

--- webshop-frontend/src/components/LogIn.jsx
+++ webshop-frontend/src/components/LogIn.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   TextField,
   Button,
@@ -34,16 +34,54 @@ const Login = () => {
   // Import the custom theme from theme.js
   const theme = useTheme();
 
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await fetch("http://localhost:8080/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        const { token } = await response.json();
+        // Store the token in local storage or a state management library (e.g., Redux)
+        localStorage.setItem("token", token);
+      } else {
+        // Handle error response
+        console.error("Login failed");
+      }
+    } catch (err) {
+      console.error("Error during login:", err);
+    }
+  };
+
   return (
     <Container maxWidth="lg" sx={{ display: "flex", justifyContent: "center" }}>
       <Box sx={{ mt: "4rem" }}>
-        <LoginBox elevation={3}>
+        <LoginBox elevation={3} component="form" onSubmit={handleSubmit}>
           <Typography variant="h4" sx={{ marginBottom: "2rem" }}>
             Login
           </Typography>
-          <StyledTextField label="Email" type="email" />
-          <StyledTextField label="Password" type="password" />
-          <StyledButton variant="contained" color="primary">
+          <StyledTextField
+            label="Email"
+            type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <StyledTextField
+            label="Password"
+            type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <StyledButton type="submit" variant="contained" color="primary">
             Login
           </StyledButton>
           <Typography
