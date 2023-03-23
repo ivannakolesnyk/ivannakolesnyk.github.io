5ccd8a2b74b8549025b90e5e52bd1553531e22cf
Mark Kour <mark.kour@icloud.com>
2023-03-23 07:52:09 +0100

    Create the header of the menu page

--- webshop-frontend/src/components/Menu.jsx
+++ webshop-frontend/src/components/Menu.jsx
@@ -1,47 +1,45 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Button,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemText,
+  Typography,
+} from "@mui/material";
 import { useTheme } from "@mui/material/styles";
+import SearchBar from "./SearchBar";
+import { Stack } from "@mui/system";
 
 const Menu = () => {
   const theme = useTheme();
 
   return (
-    <Box sx={{ p: 2, color: theme.palette.primary.contrastText }}>
-      <Typography variant="h3" sx={{ mb: 2 }}>
-        Our Menu
-      </Typography>
-      <Typography variant="h6" sx={{ mb: 1 }}>
-        Coffee
-      </Typography>
-      <Typography variant="body1" sx={{ mb: 2 }}>
-        - Espresso
-        <br />
-        - Americano
-        <br />
-        - Latte
-        <br />
-        - Cappuccino
-        <br />- Macchiato
-      </Typography>
-      <Typography variant="h6" sx={{ mb: 1 }}>
-        Tea
-      </Typography>
-      <Typography variant="body1" sx={{ mb: 2 }}>
-        - Black tea
-        <br />
-        - Green tea
-        <br />- Herbal tea
-      </Typography>
-      <Typography variant="h6" sx={{ mb: 1 }}>
-        Pastries
-      </Typography>
-      <Typography variant="body1" sx={{ mb: 2 }}>
-        - Croissant
-        <br />
-        - Pain au chocolat
-        <br />- Cinnamon roll
-      </Typography>
-    </Box>
+    <>
+      <Box
+        sx={{
+          padding: "6.4rem 13.2rem",
+          bgcolor: theme.palette.primary.main,
+        }}
+      >
+        <Box
+          sx={{
+            color: theme.palette.primary.contrastText,
+            width: "50%",
+          }}
+        >
+          <Typography variant="h1" mb={"2rem"}>
+            Menu
+          </Typography>
+          <Typography variant="body1" mb={"2rem"}>
+            Indulge in the rich flavors of our handcrafted coffee creations -
+            One sip, and you'll be hooked!
+          </Typography>
+          <SearchBar />
+        </Box>
+      </Box>
+    </>
   );
 };
 
