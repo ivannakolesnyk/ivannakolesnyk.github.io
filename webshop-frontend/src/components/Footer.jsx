61b0b003096295dccf264ec5733231451db25e55
Simon Gjøen <s_gjoen@hotmail.com>
2023-03-25 11:32:47 +0100

    Fix error message from Chrome
    Error message because a grid container can not have "xs prop". It's fixed now.

--- webshop-frontend/src/components/Footer.jsx
+++ webshop-frontend/src/components/Footer.jsx
@@ -30,137 +30,144 @@ const Footer = () => {
       }}
     >
       <Grid item xs={3}></Grid>
-      <Grid container xs={6} sx={{ pb: 2 }}>
-        <Grid
-          item
-          xs={12}
-          md={6}
-          lg={4}
-          sx={{
-            display: "flex",
-            justifyContent: { xs: "center", md: "left", lg: "left" },
+      <Grid item xs={6}>
+        <Grid container sx={{ pb: 2 }}>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            lg={4}
+            sx={{
+              display: "flex",
+              justifyContent: { xs: "center", md: "left", lg: "left" },
 
-            pb: 1,
-          }}
-        >
-          <Box sx={{ textAlign: "left", width: "fit-content" }}>
-            <Typography variant="body2" sx={{ p: 1 }}>
-              Find us: <br />
-              <Link component={Link} to="/findus">
-                Larsgårdsvegen 2<br />
-                6009 Ålesund
+              pb: 1,
+            }}
+          >
+            <Box sx={{ textAlign: "left", width: "fit-content" }}>
+              <Typography variant="body2" sx={{ p: 1 }}>
+                Find us: <br />
+                <Link component={Link} to="/findus">
+                  Larsgårdsvegen 2<br />
+                  6009 Ålesund
+                  <br />
+                </Link>
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            lg={4}
+            sx={{
+              display: "flex",
+              justifyContent: { xs: "center", md: "right", lg: "center" },
+
+              pb: 1,
+            }}
+          >
+            <Box sx={{ textAlign: "left", width: "fit-content" }}>
+              <Typography variant="body2" sx={{ p: 1 }}>
+                Contact information:
                 <br />
-              </Link>
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={6}
-          lg={4}
-          sx={{
-            display: "flex",
-            justifyContent: { xs: "center", md: "right", lg: "center" },
-
-            pb: 1,
-          }}
-        >
-          <Box sx={{ textAlign: "left", width: "fit-content" }}>
-            <Typography variant="body2" sx={{ p: 1 }}>
-              Contact information:
-              <br />
-              <a
-                href="mailto:monoka@gmail.com"
+                <a
+                  href="mailto:monoka@gmail.com"
+                  target="_blank"
+                  rel="noreferrer"
+                >
+                  monoka@gmail.com
+                </a>
+                <br />
+                +4795746534
+              </Typography>
+            </Box>
+          </Grid>
+          <Grid
+            item
+            xs={12}
+            md={6}
+            lg={4}
+            sx={{
+              display: "flex",
+              justifyContent: { xs: "center", md: "left", lg: "right" },
+            }}
+          >
+            <Box sx={{ textAlign: "left", width: "fit-content" }}>
+              <Typography
+                variant="body2"
+                sx={{ pl: 1, pt: 1, pr: 1, pb: 0, mb: 0 }}
+              >
+                Follow us:
+              </Typography>
+              <IconButton
+                href="https://www.facebook.com/"
                 target="_blank"
                 rel="noreferrer"
+                aria-label="Facebook"
               >
-                monoka@gmail.com
-              </a>
-              <br />
-              +4795746534
-            </Typography>
-          </Box>
-        </Grid>
-        <Grid
-          item
-          xs={12}
-          md={6}
-          lg={4}
-          sx={{
-            display: "flex",
-            justifyContent: { xs: "center", md: "left", lg: "right" },
-          }}
-        >
-          <Box sx={{ textAlign: "left", width: "fit-content" }}>
-            <Typography
-              variant="body2"
-              sx={{ pl: 1, pt: 1, pr: 1, pb: 0, mb: 0 }}
-            >
-              Follow us:
-            </Typography>
-            <IconButton
-              href="https://www.facebook.com/"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="Facebook"
-            >
-              <FacebookIcon
-                sx={{
-                  fontSize: "2.5rem",
-                  color: theme.palette.secondary.main,
-                }}
-              />
-            </IconButton>
-            <IconButton
-              href="https://www.instagram.com/"
-              target="_blank"
-              rel="noreferrer"
-              aria-label="Instagram"
-            >
-              <InstagramIcon
-                sx={{ fontSize: "2.5rem", color: theme.palette.secondary.main }}
-              />
-            </IconButton>
-          </Box>
+                <FacebookIcon
+                  sx={{
+                    fontSize: "2.5rem",
+                    color: theme.palette.secondary.main,
+                  }}
+                />
+              </IconButton>
+              <IconButton
+                href="https://www.instagram.com/"
+                target="_blank"
+                rel="noreferrer"
+                aria-label="Instagram"
+              >
+                <InstagramIcon
+                  sx={{
+                    fontSize: "2.5rem",
+                    color: theme.palette.secondary.main,
+                  }}
+                />
+              </IconButton>
+            </Box>
+          </Grid>
         </Grid>
       </Grid>
       <Grid item xs={3}></Grid>
 
       <Grid item xs={3}></Grid>
 
-      <Grid container xs={6}>
-        <Grid
-          item
-          xs={12}
-          sx={{
-            textAlign: "left",
-            p: 1,
-          }}
-        >
-          <Typography variant="body2">
-            This website is a result of a university group project, performed in
-            the course{" "}
-            <a
-              href="https://www.ntnu.edu/studies/courses/IDATA2301#tab=omEmnet"
-              target="_blank"
-              rel="noreferrer"
-              className="footer-link"
-            >
-              IDATA2301 Web technologies
-            </a>
-            , at{" "}
-            <a
-              href="https://www.ntnu.edu/"
-              target="_blank"
-              rel="noreferrer"
-              className="footer-link"
-            >
-              NTNU
-            </a>
-            . All the information provided here is a result of imagination. Any
-            resemblance with real companies or products is a coincidence
-          </Typography>
+      <Grid item xs={6}>
+        <Grid container>
+          <Grid
+            item
+            xs={12}
+            sx={{
+              textAlign: "left",
+              p: 1,
+            }}
+          >
+            <Typography variant="body2">
+              This website is a result of a university group project, performed
+              in the course{" "}
+              <a
+                href="https://www.ntnu.edu/studies/courses/IDATA2301#tab=omEmnet"
+                target="_blank"
+                rel="noreferrer"
+                className="footer-link"
+              >
+                IDATA2301 Web technologies
+              </a>
+              , at{" "}
+              <a
+                href="https://www.ntnu.edu/"
+                target="_blank"
+                rel="noreferrer"
+                className="footer-link"
+              >
+                NTNU
+              </a>
+              . All the information provided here is a result of imagination.
+              Any resemblance with real companies or products is a coincidence
+            </Typography>
+          </Grid>
         </Grid>
       </Grid>
 
