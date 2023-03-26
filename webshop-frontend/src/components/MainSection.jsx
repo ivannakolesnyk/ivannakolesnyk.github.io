c0e5c7f3caa3d604d5d7fad5004b893736404379
ivannakolesnyk <113033729+ivannakolesnyk@users.noreply.github.com>
2023-03-26 16:44:12 +0200

    Devide sections to components

---
+++ webshop-frontend/src/components/MainSection.jsx
@@ -0,0 +1,59 @@
+import { Typography, Button, Stack, Paper, Grid } from "@mui/material";
+import { useTheme } from "@mui/material/styles";
+import backgroundImage from "../assets/img/home.png";
+
+const MainSection = () => {
+  const theme = useTheme();
+
+  return (
+    <Stack
+      height={"70vh"}
+      py={12}
+      px={8}
+      sx={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <Grid
+        container
+        justifyContent="left"
+        alignItems="center"
+        sx={{ height: "100%" }}
+      >
+        <Grid item xs={12} md={8} lg={6}>
+          <Paper sx={{ p: 7, maxWidth: 500, backgroundColor: "primary.main" }}>
+            <Stack spacing={3} alignItems="left">
+              <Typography
+                fontWeight={600}
+                variant={"h1"}
+                color={"primary.contrastText"}
+              >
+                ENJOY YOUR OWN COFFEE AT HOME
+              </Typography>
+              <Typography fontWeight={300} variant={"body2"}>
+                A variety of products to make your unique drink yourself. From
+                grinders to filters, milk frothers to pour-over kettles, our
+                range of coffee-making products lets you unleash your inner
+                barista and enjoy coffee just the way you like it.
+              </Typography>
+              <Button
+                variant={"contained"}
+                color={"secondary"}
+                sx={{
+                  maxWidth: "200px",
+                  fontSize: "2.2rem",
+                  color: "white",
+                }}
+              >
+                ORDER NOW
+              </Button>
+            </Stack>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Stack>
+  );
+};
+export { MainSection };
