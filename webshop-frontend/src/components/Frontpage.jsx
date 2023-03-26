d3860640b630d8691225fecd9b152ef109a79b54
ivannakolesnyk <113033729+ivannakolesnyk@users.noreply.github.com>
2023-03-26 20:21:54 +0200

    Add AboutUs section

--- webshop-frontend/src/components/Frontpage.jsx
+++ webshop-frontend/src/components/Frontpage.jsx
@@ -1,11 +1,14 @@
 import { MainSection } from "../components/MainSection.jsx";
 import { BannerSection } from "../components/BannerSection.jsx";
+import { AboutUsSection } from "../components/AboutUsSection.jsx";
 
 const Frontpage = () => {
   return (
     <div>
       <MainSection />
+
       <BannerSection />
+      <AboutUsSection />
     </div>
   );
 };
