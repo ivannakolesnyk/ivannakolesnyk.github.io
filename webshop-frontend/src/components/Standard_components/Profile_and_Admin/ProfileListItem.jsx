7ffb9b621a37ec2f27d9b17f16da74b32078a3fa
Simon Gjøen <s_gjoen@hotmail.com>
2023-03-31 16:46:05 +0200

    Update ProfileListItem.jsx
    Fix typo and Prettier reformat.

--- webshop-frontend/src/components/Standard_components/Profile_and_Admin/ProfileListItem.jsx
+++ webshop-frontend/src/components/Standard_components/Profile_and_Admin/ProfileListItem.jsx
@@ -1,13 +1,13 @@
-import {ListItem, ListItemIcon, ListItemText} from "@mui/material";
+import { ListItem, ListItemIcon, ListItemText } from "@mui/material";
 import React from "react";
 
 /**
  * A custom ListItem component that takes in `icon`, `primary` and 'secondary' as props.
- * It's use is in the the Card which shows the personal informartion of a user.
+ * It's use is in the Card which shows the personal information of a user.
  */
-export const ProfileListItem = ({icon, primary, secondary}) => (
-    <ListItem>
-        <ListItemIcon>{icon}</ListItemIcon>
-        <ListItemText primary={primary} secondary={secondary}/>
-    </ListItem>
+export const ProfileListItem = ({ icon, primary, secondary }) => (
+  <ListItem>
+    <ListItemIcon>{icon}</ListItemIcon>
+    <ListItemText primary={primary} secondary={secondary} />
+  </ListItem>
 );
