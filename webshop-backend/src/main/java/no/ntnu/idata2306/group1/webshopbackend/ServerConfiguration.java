9677f81a9c42edfb5fce7a20c21bccf4069e8782
Mark Kour <mark.kour@icloud.com>
2023-04-01 11:30:57 +0200

    Add server configuration to allow HTTP requests

---
+++ webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/ServerConfiguration.java
@@ -0,0 +1,26 @@
+package no.ntnu.idata2306.group1.webshopbackend;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Represents the configuration of the server.
+ */
+@Configuration
+public class ServerConfiguration {
+
+  /**
+   * Allows HTTP methods.
+   */
+  @Bean
+  public WebMvcConfigurer corsConfigurer() {
+    return new WebMvcConfigurer() {
+      @Override
+      public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**").allowedMethods("HEAD", "GET", "PUT", "POST", "DELETE", "PATCH");
+      }
+    };
+  }
+}
