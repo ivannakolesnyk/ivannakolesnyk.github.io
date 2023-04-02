0c6610c534bc5379c98e308ed2df43fad4b37401
Mark Kour <mark.kour@icloud.com>
2023-04-02 19:39:44 +0200

    Add default constructor to avoid problem when deserializing JSON

--- webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/model/data/Category.java
+++ webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/model/data/Category.java
@@ -1,6 +1,6 @@
 /*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this
+ * license Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package no.ntnu.idata2306.group1.webshopbackend.model.data;
 
@@ -20,7 +20,17 @@ public class Category {
     private int id;
     private String name;
 
+    public Category() {}
+
     public Category(String name) {
         this.name = name;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
     }
 }
