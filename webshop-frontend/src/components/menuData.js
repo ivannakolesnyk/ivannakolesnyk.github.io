fa7f77aaacab64bbb23cb9b6bc5f8a3990187962
Mark Kour <mark.kour@icloud.com>
2023-04-09 11:46:57 +0200

    refactor: Extract menu and menuBar to menuData file

---
+++ webshop-frontend/src/components/menuData.js
@@ -0,0 +1,98 @@
+export const menuBar = ["hot drinks", "cold drinks", "food", "pastries"];
+
+export const menu = [
+  {
+    name: "Hot drinks",
+    products: [
+      {
+        id: 1,
+        productName: "Cortado",
+        sale: false,
+        imagePath: "menu/hot_drink.png",
+      },
+      {
+        id: 2,
+        productName: "Gibraltar",
+        sale: true,
+        imagePath: "menu/hot_drink.png",
+      },
+      {
+        id: 3,
+        productName: "Breve",
+        sale: false,
+        imagePath: "menu/hot_drink.png",
+      },
+      {
+        id: 4,
+        productName: "Latte",
+        sale: false,
+        imagePath: "menu/hot_drink.png",
+      },
+      {
+        id: 5,
+        productName: "Americano",
+        sale: true,
+        imagePath: "menu/hot_drink.png",
+      },
+      {
+        id: 6,
+        productName: "Cappuccino",
+        sale: true,
+        imagePath: "menu/hot_drink.png",
+      },
+      {
+        id: 7,
+        productName: "Macchiato",
+        sale: true,
+        imagePath: "menu/hot_drink.png",
+      },
+    ],
+  },
+  {
+    name: "Cold drinks",
+    products: [
+      {
+        id: 1,
+        productName: "Iced Mocha",
+        sale: false,
+        imagePath: "menu/cold_drink.png",
+      },
+      {
+        id: 2,
+        productName: "Banana Milk Coffee",
+        sale: true,
+        imagePath: "menu/cold_drink.png",
+      },
+      {
+        id: 3,
+        productName: "Creamy Iced Coffee",
+        sale: false,
+        imagePath: "menu/cold_drink.png",
+      },
+      {
+        id: 4,
+        productName: "Iced Caramel Macchiato",
+        sale: false,
+        imagePath: "menu/cold_drink.png",
+      },
+      {
+        id: 5,
+        productName: "Cold-Brewed Coffee",
+        sale: true,
+        imagePath: "menu/cold_drink.png",
+      },
+      {
+        id: 6,
+        productName: "Chocolate-y Iced Mocha",
+        sale: true,
+        imagePath: "menu/cold_drink.png",
+      },
+      {
+        id: 7,
+        productName: "Ice special",
+        sale: true,
+        imagePath: "menu/cold_drink.png",
+      },
+    ],
+  },
+];
