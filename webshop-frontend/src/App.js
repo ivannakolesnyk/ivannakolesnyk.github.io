4a1d9454ab70d615f3206f18b712d1f9614a0d9f
Mark Kour <mark.kour@icloud.com>
2023-04-10 14:56:38 +0200

    feat: Restrict access to /profile route and its subroutes

--- webshop-frontend/src/App.js
+++ webshop-frontend/src/App.js
@@ -1,6 +1,6 @@
 import { Box } from "@mui/material";
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React, { useContext } from "react";
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
 import AboutUs from "./components/AboutUs";
 import Admin from "./components/Admin/Admin";
 import AdminChangePassword from "./components/Admin/AdminChangePW";
@@ -23,8 +23,11 @@ import ProfileEdit from "./components/Profile/ProfileEdit";
 import ProfileViewOrders from "./components/Profile/ProfileViewOrders";
 import ShoppingCart from "./components/ShoppingCart";
 import NotFound from "./components/Standard_components/NotFound";
+import { AuthContext } from "./context/AuthContext";
 
 function App() {
+  const { loggedIn } = useContext(AuthContext);
+
   return (
     <BrowserRouter>
       <Box display="flex" flexDirection="column" minHeight="100vh">
@@ -40,10 +43,20 @@ function App() {
             <Route path="/login" element={<LogIn />} />
             <Route path="/register" element={<RegisterNewUser />} />
             <Route path="/shoppingcart" element={<ShoppingCart />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/profile/edit" element={<ProfileEdit />} />
-            <Route path="/profile/changepw" element={<ProfileChangePW />} />
-            <Route path="/profile/vieworders" element={<ProfileViewOrders />} />
+            {loggedIn ? (
+              <>
+                <Route path="/profile" element={<Profile />} />
+                <Route
+                  path="/profile/vieworders"
+                  element={<ProfileViewOrders />}
+                />
+                <Route path="/profile/edit" element={<ProfileEdit />} />
+                <Route path="/profile/changepw" element={<ProfileChangePW />} />
+              </>
+            ) : (
+              <Route path="/profile/*" element={<Navigate to="/login" />} />
+            )}
+            {/* TODO: restrict access to admin too. (user.role) */}
             <Route path="/admin" element={<Admin />} />
             <Route path="/admin/edit" element={<AdminEditProfilePage />} />
             <Route path="/admin/changepw" element={<AdminChangePassword />} />
