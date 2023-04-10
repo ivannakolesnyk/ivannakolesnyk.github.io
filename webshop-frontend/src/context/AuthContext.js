e5b459642f11686d2541a305072718184c6961a4
Simon Gjøen <s_gjoen@hotmail.com>
2023-04-10 15:10:13 +0200

    Update AuthContext.js
    Small cosmetic fix to formatting

--- webshop-frontend/src/context/AuthContext.js
+++ webshop-frontend/src/context/AuthContext.js
@@ -3,12 +3,12 @@ import React, { useState } from "react";
 export const AuthContext = React.createContext();
 
 /**
-
-The AuthProvider component provides an AuthContext to all child components.
-It manages the user's login state and provides functions for logging in and logging out.
-@param {object} children - The child components to be wrapped with the AuthContext.
-@returns {JSX.Element} - The JSX code for the AuthProvider component.
-*/
+ *
+ * The AuthProvider component provides an AuthContext to all child components.
+ * It manages the user's login state and provides functions for logging in and logging out.
+ * @param {object} children - The child components to be wrapped with the AuthContext.
+ * @returns {JSX.Element} - The JSX code for the AuthProvider component.
+ */
 export const AuthProvider = ({ children }) => {
   const [loggedIn, setLoggedIn] = useState(() => {
     const savedLoggedIn = localStorage.getItem("loggedIn");
