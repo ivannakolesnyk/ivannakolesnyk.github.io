5688f1de0d7f0f8a3d494e26517e4911743d465e
Mark Kour <mark.kour@icloud.com>
2023-04-11 13:47:03 +0200

    feat: Implement UserProfileDto

--- webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/dto/UserProfileDto.java
+++ webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/dto/UserProfileDto.java
@@ -4,9 +4,55 @@ package no.ntnu.idata2306.group1.webshopbackend.dto;
  * Data transfer object (DTO) for submitting changes to user profile data.
  */
 public class UserProfileDto {
+  private String name;
+  private String phone_number;
+  private int postal_code;
+  private String address;
+  private String city;
 
   public UserProfileDto() {
   }
 
+  public String getName() {
+    return name;
+  }
 
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getPhone_number() {
+    return phone_number;
+  }
+
+  public void setPhone_number(String phone_number) {
+    this.phone_number = phone_number;
+  }
+
+  public int getPostal_code() {
+    return postal_code;
+  }
+
+  public void setPostal_code(int postal_code) {
+    this.postal_code = postal_code;
+  }
+
+  public String getAddress() {
+    return address;
+  }
+
+  public void setAddress(String address) {
+    this.address = address;
+  }
+
+  public String getCity() {
+    return city;
+  }
+
+  public void setCity(String city) {
+    this.city = city;
+  }
 }
+
+
+
