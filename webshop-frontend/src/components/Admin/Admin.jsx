9038bf038fafbff227bce2d2a576269563fd7f75
Mark Kour <mark.kour@icloud.com>
2023-04-13 07:23:20 +0200

    feat: Render admin profile data dynamically

--- webshop-frontend/src/components/Admin/Admin.jsx
+++ webshop-frontend/src/components/Admin/Admin.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import {
   Button,
@@ -11,6 +11,10 @@ import { useTheme } from "@mui/material/styles";
 import StandardCenteredBox from "../Standard_components/StandardCenteredBox";
 import StandardCenteredCard from "../Standard_components/StandardCenteredCard";
 import { ProfileInformation } from "../Standard_components/Profile_and_Admin/ProfileInformation";
+import cookie from "cookie";
+import Loading from "../Standard_components/Loading";
+import InternalError from "../Standard_components/InternalError";
+import { AuthContext } from "../../context/AuthContext";
 
 /**
  *
@@ -43,11 +47,59 @@ const sections = [
 ];
 const Admin = () => {
   const theme = useTheme();
+  const { getJwtPayload } = useContext(AuthContext);
+  const [profileData, setProfileData] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [fetchError, setFetchError] = useState(false);
+
+  useEffect(() => {
+    const fetchProfileData = async () => {
+      try {
+        const payload = getJwtPayload();
+        if (payload) {
+          const userEmail = payload.sub; // Replace 'sub' with the claim name containing the user's email
+          const jwt = cookie.parse(document.cookie).jwt;
+          console.log(jwt);
+          const response = await fetch(
+            `http://localhost:8080/api/users/${userEmail}`,
+            {
+              headers: {
+                "Content-Type": "application/json",
+                Authorization: `Bearer ${jwt}`,
+              },
+            }
+          );
+
+          if (response.ok) {
+            const data = await response.json();
+            setProfileData({ ...data, userEmail });
+          } else {
+            console.error("Error fetching profile data:", response.status);
+            setFetchError(true);
+          }
+        }
+      } catch (error) {
+        console.error("Error fetching profile data:", error);
+        setFetchError(true);
+      }
+      setIsLoading(false);
+    };
+
+    fetchProfileData();
+  }, [getJwtPayload]);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (fetchError) {
+    return <InternalError />;
+  }
 
   return (
     <StandardCenteredBox>
       <StandardCenteredCard>
-        <ProfileInformation theme={theme} />
+        <ProfileInformation theme={theme} profileData={profileData} />
         <CardActions sx={{ justifyContent: "flex-end" }}>
           <Button component={Link} to="/admin/edit" variant="contained">
             Edit Profile
