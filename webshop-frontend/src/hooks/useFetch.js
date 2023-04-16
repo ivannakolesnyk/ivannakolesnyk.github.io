5d38d75b8f4dcbe6c20e1a92124ffc1c629a35e3
Mark Kour <mark.kour@icloud.com>
2023-04-16 12:31:26 +0200

    feat: enhance hook to support various request methods and headers
    - Update useFetch hook to accept method, headers, and requestBody parameters
    - Handle different HTTP methods and request payloads
    - Update error handling to check response status
    - Update documentation and example usage accordingly

--- webshop-frontend/src/hooks/useFetch.js
+++ webshop-frontend/src/hooks/useFetch.js
@@ -2,15 +2,20 @@ import axios from "axios";
 import { useCallback, useEffect, useState } from "react";
 
 /**
- * A custom React hook to fetch data from a specified API endpoint.
+ * A custom React hook to send a request to a specified API endpoint and handle its response.
  *
- * @param {string} endpoint - The API endpoint to fetch data from (without the base URL).
- * @param {object} [query] - An optional object containing key-value pairs to be used as query parameters in the GET request.
- * @returns {object} An object containing the fetched data, the loading state, any error encountered during the fetch, and a `refetch` function to trigger a re-fetch manually.
+ * @param {string} method - The HTTP method to use for the request (e.g., "GET", "POST", "PUT", "DELETE").
+ * @param {string} endpoint - The API endpoint for the request (without the base URL).
+ * @param {object} headers - An object containing key-value pairs to be used as headers in the request.
+ * @param {object} [query] - An optional object containing key-value pairs to be used as query parameters in the request.
+ * @param {object} requestBody - An object containing the request payload.
+ * @param {boolean} [autoFetch=true] - An optional boolean flag to determine if the request should be sent automatically when the hook is called.
+ * @param {string} [baseURL="http://localhost:8080/api/"] - An optional base URL for the API endpoint.
+ * @returns {object} An object containing the fetched data, the loading state, any error encountered during the fetch, a `refetch` function to trigger a re-fetch manually, and a `fetched` boolean flag indicating whether the data has been fetched.
  *
  * @example
  * import useFetch from "./useFetch";
- * const { data, isLoading, error, refetch } = useFetch("products", { name: "Coffee" });
+ * const { data, isLoading, error, refetch, fetched } = useFetch("GET", "products", { "Content-Type": "application/json" }, { name: "Coffee" }, {}, false);
  */
 const useFetch = (
   method,
@@ -18,41 +23,38 @@ const useFetch = (
   headers,
   query,
   requestBody,
-  autoFetch = true
+  autoFetch = true,
+  baseURL = "http://localhost:8080/api/"
 ) => {
   const [data, setData] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(null);
   const [fetched, setFetched] = useState(false);
-  const [showError, setShowError] = useState(false);
 
   const fetchData = useCallback(async () => {
     setIsLoading(true);
     const options = {
       method: method,
-      url: `http://localhost:8080/api/${endpoint}`,
+      url: `${baseURL}${endpoint}`,
       headers: { ...headers },
       params: { ...query },
       data: requestBody,
     };
 
     try {
-      const respond = await axios.request(options);
-      setData(respond.data);
-      setFetched(true);
-      setIsLoading(false);
+      const response = await axios.request(options);
+      if (response.status >= 200 && response.status < 300) {
+        setData(response.data);
+        setFetched(true);
+      } else {
+        setError(new Error(`Server responded with status ${response.status}`));
+      }
     } catch (error) {
       setError(error);
-      // Remove alert and set showError to true
-      setShowError(true);
     } finally {
       setIsLoading(false);
     }
-  }, [method, endpoint, headers, query, requestBody]);
-
-  const dismissError = () => {
-    setShowError(false);
-  };
+  }, [method, endpoint, headers, query, requestBody, baseURL]);
 
   useEffect(() => {
     if (autoFetch) {
@@ -65,7 +67,7 @@ const useFetch = (
     fetchData();
   };
 
-  return { data, isLoading, error, refetch, showError, dismissError, fetched };
+  return { data, isLoading, error, refetch, fetched };
 };
 
 export default useFetch;
