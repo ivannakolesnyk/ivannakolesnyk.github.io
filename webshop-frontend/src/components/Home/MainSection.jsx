72a93786a895d30bd34ff25d35f6cb6b162e3960
ivannakolesnyk <113033729+ivannakolesnyk@users.noreply.github.com>
2023-04-20 00:25:27 +0200

    fix layout, make it more responsive

--- webshop-frontend/src/components/Home/MainSection.jsx
+++ webshop-frontend/src/components/Home/MainSection.jsx
@@ -8,46 +8,45 @@ const MainSection = () => {
       minHeight={"70vh"}
       py={12}
       px={8}
+      bgcolor={"red"}
+      justifyContent={"center"}
+      alignItems={"flex-start"}
       sx={{
         backgroundImage: `url(${backgroundImage})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <Grid container alignItems="center" sx={{ height: "100%" }}>
-        <Grid item xs={12} md={8} lg={6}>
-          <Paper sx={{ p: 7, maxWidth: 500, backgroundColor: "primary.main" }}>
-            <Stack spacing={3} alignItems="left">
-              <Typography
-                fontWeight={600}
-                variant={"h1"}
-                color={"primary.contrastText"}
-              >
-                ENJOY YOUR OWN COFFEE AT HOME
-              </Typography>
-              <Typography fontWeight={300} variant={"body2"}>
-                A variety of products to make your unique drink yourself. From
-                grinders to filters, milk frothers to pour-over kettles, our
-                range of coffee-making products lets you unleash your inner
-                barista and enjoy coffee just the way you like it.
-              </Typography>
-              <Link to="/product">
-                <Button
-                  variant={"contained"}
-                  color={"secondary"}
-                  sx={{
-                    maxWidth: "200px",
-                    fontSize: "2.2rem",
-                    color: "white",
-                  }}
-                >
-                  ORDER NOW
-                </Button>
-              </Link>
-            </Stack>
-          </Paper>
-        </Grid>
-      </Grid>
+      <Paper sx={{ p: 7, maxWidth: 500, backgroundColor: "primary.main" }}>
+        <Stack spacing={3} alignItems="left">
+          <Typography
+            fontWeight={600}
+            variant={"h1"}
+            color={"primary.contrastText"}
+          >
+            ENJOY YOUR OWN COFFEE AT HOME
+          </Typography>
+          <Typography fontWeight={300} variant={"body2"}>
+            A variety of products to make your unique drink yourself. From
+            grinders to filters, milk frothers to pour-over kettles, our range
+            of coffee-making products lets you unleash your inner barista and
+            enjoy coffee just the way you like it.
+          </Typography>
+          <Link to="/product">
+            <Button
+              variant={"contained"}
+              color={"secondary"}
+              sx={{
+                maxWidth: "200px",
+                fontSize: "2.2rem",
+                color: "white",
+              }}
+            >
+              ORDER NOW
+            </Button>
+          </Link>
+        </Stack>
+      </Paper>
     </Stack>
   );
 };
