d1e73828a6cfced16bbbd0e6fd757a8626f9df9a
ivannakolesnyk <113033729+ivannakolesnyk@users.noreply.github.com>
2023-04-20 00:25:16 +0200

    fix layout, make it more responsive

--- webshop-frontend/src/components/Home/BannerSection.jsx
+++ webshop-frontend/src/components/Home/BannerSection.jsx
@@ -5,47 +5,40 @@ import banner from "../../assets/img/banner.png";
 const BannerSection = () => {
   return (
     <Stack
-      height={"80vh"}
-      py={12}
+      minHeight={"80vh"}
+      py={24}
       px={8}
+      alignItems={"flex-start"}
+      justifyContent={"center"}
       sx={{
         backgroundImage: `url(${banner})`,
         backgroundSize: "cover",
         backgroundPosition: "center",
       }}
     >
-      <Grid
-        container
-        justifyContent="left"
-        alignItems="center"
-        sx={{ height: "100%" }}
-      >
-        <Grid item xs={12} md={8} lg={6}>
-          <Paper sx={{ p: 7, maxWidth: 400, backgroundColor: "extra" }}>
-            <Stack spacing={4} alignItems="left">
-              <Typography fontWeight={600} variant={"h0"} color={"secondary"}>
-                3 for 2
-              </Typography>
-              <Typography color={"secondary"}>
-                Mix and match or find exactly what you want
-              </Typography>
-              <Link to="/product">
-                <Button
-                  variant={"contained"}
-                  color={"secondary"}
-                  sx={{
-                    maxWidth: "160px",
-                    fontSize: "2rem",
-                    color: "white",
-                  }}
-                >
-                  ORDER NOW
-                </Button>
-              </Link>
-            </Stack>
-          </Paper>
-        </Grid>
-      </Grid>
+      <Paper sx={{ p: 7, maxWidth: 400, backgroundColor: "extra" }}>
+        <Stack spacing={4}>
+          <Typography fontWeight={600} variant={"h0"} color={"secondary"}>
+            3 for 2
+          </Typography>
+          <Typography color={"secondary"}>
+            Mix and match or find exactly what you want
+          </Typography>
+          <Link to="/product">
+            <Button
+              variant={"contained"}
+              color={"secondary"}
+              sx={{
+                maxWidth: "160px",
+                fontSize: "2rem",
+                color: "white",
+              }}
+            >
+              ORDER NOW
+            </Button>
+          </Link>
+        </Stack>
+      </Paper>
     </Stack>
   );
 };
