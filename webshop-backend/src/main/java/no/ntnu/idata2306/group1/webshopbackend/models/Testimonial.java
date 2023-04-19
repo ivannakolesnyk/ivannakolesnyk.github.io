ce43027fe2944a2f10c7f90232bfd8428f334330
Julian Montague <juliangm@stud.ntnu.no>
2023-04-19 16:11:13 +0200

    Remove unused imports in Testimonial
    Mark removed the User column in [this commit][1], but forgot to remove
    the imports that were no longer needed.
    
    [1]: bb05cb5e5581e6770608d998b5a65bee4b153bcd

--- webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/models/Testimonial.java
+++ webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/models/Testimonial.java
@@ -6,11 +6,8 @@ package no.ntnu.idata2306.group1.webshopbackend.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 
 /**
  * A testimonial (review) from a customer.
