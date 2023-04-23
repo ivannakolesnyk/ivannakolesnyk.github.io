b940cef2db1ecf0e9ece89bc708aef05fb70d03b
Mark Kour <mark.kour@icloud.com>
2023-04-23 11:53:06 +0200

    docs: Add JSDoc to useAuthHeaders & usePaymentSuccess hooks

--- webshop-frontend/src/hooks/useAuthHeaders.js
+++ webshop-frontend/src/hooks/useAuthHeaders.js
@@ -2,6 +2,14 @@ import { useMemo } from "react";
 import cookie from "cookie";
 import jwt_decode from "jwt-decode";
 
+/**
+ * A custom React hook that parses JWT from the cookie, decodes the user's email,
+ * and creates the headers with the JWT for authenticated requests.
+ *
+ * @returns {Object} An object containing the headers and the user's email.
+ * @property {Object} headers - The headers object containing the Content-Type and Authorization fields.
+ * @property {string} userEmail - The user's email decoded from the JWT payload.
+ */
 export const useAuthHeaders = () => {
     const jwt = cookie.parse(document.cookie).jwt;
     const payload = jwt_decode(jwt);
--- webshop-frontend/src/hooks/usePaymentSuccess.js
+++ webshop-frontend/src/hooks/usePaymentSuccess.js
@@ -1,6 +1,12 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
+/**
+ * A custom React hook that checks for the "paymentSuccess" query parameter
+ * in the URL, clears the cart, and removes the query parameter if found.
+ *
+ * @param {Function} clearCart - The function to clear the cart when paymentSuccess is true.
+ */
 export const usePaymentSuccess = (clearCart) => {
     const navigate = useNavigate();
 
