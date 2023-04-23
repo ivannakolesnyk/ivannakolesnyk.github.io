633b5b5eb29ccabd7fa17bd8dac48b1354cde0dc
Mark Kour <mark.kour@icloud.com>
2023-04-23 14:33:37 +0200

    fix: Prevent decoding of JWT when it's undefined

--- webshop-frontend/src/hooks/useAuthHeaders.js
+++ webshop-frontend/src/hooks/useAuthHeaders.js
@@ -12,7 +12,7 @@ import jwt_decode from "jwt-decode";
  */
 export const useAuthHeaders = () => {
     const jwt = cookie.parse(document.cookie).jwt;
-    const payload = jwt_decode(jwt);
+    const payload = jwt? jwt_decode(jwt) : "";
     const userEmail = payload ? payload.sub : ""; // Replace 'sub' with the claim name containing the user's email
 
     const headers = useMemo(
