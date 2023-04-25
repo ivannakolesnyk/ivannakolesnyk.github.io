fdd7fccc5fc1e6d7ffdb597403f169139383fd0d
Simon Gjøen <s_gjoen@hotmail.com>
2023-04-25 13:22:59 +0200

    Add swagger documentation to CategoryController

--- webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/controllers/CategoryController.java
+++ webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/controllers/CategoryController.java
@@ -10,6 +10,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import no.ntnu.idata2306.group1.webshopbackend.models.Category;
 import no.ntnu.idata2306.group1.webshopbackend.repositories.CategoryRepository;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 
 @RestController
 @RequestMapping("/categories")
@@ -27,6 +30,11 @@ public class CategoryController {
    * @param category The category object to be added.
    * @return A ResponseEntity containing the saved category object and a CREATED status.
    */
+  @Operation(summary = "Add a new category")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "201", description = "Category created successfully"),
+          @ApiResponse(responseCode = "400", description = "Invalid input data")
+  })
   @PostMapping
   public ResponseEntity<Category> addCategory(@RequestBody Category category) {
     Category savedCategory = categoryRepository.save(category);
@@ -38,10 +46,14 @@ public class CategoryController {
    *
    * @return A ResponseEntity containing the list of categories and an OK status.
    */
+  @Operation(summary = "Get all categories")
+  @ApiResponses(value = {
+          @ApiResponse(responseCode = "200", description = "Categories fetched successfully"),
+          @ApiResponse(responseCode = "404", description = "No categories found")
+  })
   @GetMapping
   public ResponseEntity<List<Category>> getCategories() {
     List<Category> categories = categoryRepository.findAll();
     return new ResponseEntity<>(categories, HttpStatus.OK);
   }
 }
-
