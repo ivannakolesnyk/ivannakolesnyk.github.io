b5afa3787dd5b92b49ec9cf0700362b8f731ff28
Simon Gjøen <s_gjoen@hotmail.com>
2023-04-26 00:17:30 +0200

    Swagger documentation for Category

--- webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/models/Category.java
+++ webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/models/Category.java
@@ -4,6 +4,7 @@
  */
 package no.ntnu.idata2306.group1.webshopbackend.models;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -14,10 +15,14 @@ import jakarta.persistence.Id;
  * @author julian
  */
 @Entity
+@Schema(description = "A category for products in the webshop")
 public class Category {
     @Id
     @GeneratedValue
+    @Schema(description = "The unique identifier of the category")
     private int id;
+
+    @Schema(description = "The name of the category")
     private String name;
 
     public Category() {}
