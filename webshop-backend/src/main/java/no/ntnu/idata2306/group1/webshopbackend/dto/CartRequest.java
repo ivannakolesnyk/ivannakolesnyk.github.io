06ef81c41a20fef264dc52349997073a20683100
Simon Gjøen <s_gjoen@hotmail.com>
2023-04-26 00:27:21 +0200

    Add swagger documentation to CartRequest

--- webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/dto/CartRequest.java
+++ webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/dto/CartRequest.java
@@ -1,11 +1,16 @@
 package no.ntnu.idata2306.group1.webshopbackend.dto;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import no.ntnu.idata2306.group1.webshopbackend.models.CartItem;
 
 import java.util.List;
 
+@Schema(description = "Represents a cart request with a list of cart items and user ID")
 public class CartRequest {
+    @Schema(description = "A list of cart items")
     private List<CartItem> cart;
+
+    @Schema(description = "The user ID associated with the cart")
     private String userId;
 
     public CartRequest() {
