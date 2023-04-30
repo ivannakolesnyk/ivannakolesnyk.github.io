08e1f5759a7826ecfecee4022d19beae3801a395
Mark Kour <mark.kour@icloud.com>
2023-04-30 19:00:04 +0200

    refactor: Gather all styles in styles.js & improve readability

--- webshop-frontend/src/components/Menu/Menu.jsx
+++ webshop-frontend/src/components/Menu/Menu.jsx
@@ -6,58 +6,54 @@ import MenuItem from "./MenuItem";
 import MenuSearchResults from "./MenuSearchResults";
 import MenuSection from "./MenuSection";
 import { menu, menuBar } from "./menuData";
+import styles from "./styles";
 
+/**
+ * Menu component represents the main menu of the application, displaying product categories
+ * and allowing users to search for products.
+ *
+ * It consists of a header with a search bar, a menu bar with navigation items, and sections with product items.
+ * When a search is performed, the component displays search results, otherwise, it displays the regular menu items.
+ */
 const Menu = () => {
   const [searchTerm, setSearchTerm] = useState("");
-  const handleSearchChange = (value) => {
-    setSearchTerm(value);
-  };
-
-  const sliceMenuItems = (products) => {
-    return products.slice(0, 3);
-  };
-
-  const renderMenuItems = (filteredProducts) => (
-    <Grid container spacing={2.5} role="list">
-      {filteredProducts.map(({ id, productName, imagePath, imageAlt }) => (
-        <Grid item key={id} xs={12} sm={6} md={4} lg={4} role="listitem">
-          <Box textAlign={"center"}>
-            <ProductCard
-              productName={productName}
-              imagePath={`${process.env.PUBLIC_URL}/assets/img/${imagePath}`}
-              imageAlt={imageAlt}
-              isClickable={false}
-            />
-          </Box>
-        </Grid>
-      ))}
-    </Grid>
-  );
-
-  const sectionRefs = menu.map(() => React.createRef());
-  const scrollToSection = (index) => {
-    sectionRefs[index].current.scrollIntoView({
-      behavior: "smooth",
-      block: "start",
-    });
-  };
-
   const [expandedCategory, setExpandedCategory] = useState(null);
 
+  const handleSearchChange = (value) => setSearchTerm(value);
+  const sectionRefs = menu.map(() => React.createRef());
+
   const handleViewAllClick = (categoryName) => {
-    if (expandedCategory === categoryName) {
-      setExpandedCategory(null);
-    } else {
-      setExpandedCategory(categoryName);
-    }
+    setExpandedCategory((prevCategory) =>
+      prevCategory === categoryName ? null : categoryName
+    );
+  };
+
+  const scrollToSection = (index) =>
+    sectionRefs[index].current.scrollIntoView(styles.scroll);
+
+  const renderMenuItems = (filteredProducts) => {
+    return (
+      <Grid container spacing={2.5} role="list">
+        {filteredProducts.map(({ id, productName, imagePath, imageAlt }) => (
+          <Grid item key={id} xs={12} sm={6} md={4} lg={4} role="listitem">
+            <Box textAlign={"center"}>
+              <ProductCard
+                productName={productName}
+                imagePath={`${process.env.PUBLIC_URL}/assets/img/${imagePath}`}
+                imageAlt={imageAlt}
+                isClickable={false}
+              />
+            </Box>
+          </Grid>
+        ))}
+      </Grid>
+    );
   };
 
   const renderCategoryItems = (categoryName, products) => {
-    if (expandedCategory === categoryName) {
-      return renderMenuItems(products);
-    } else {
-      return renderMenuItems(sliceMenuItems(products));
-    }
+    const itemsToShow =
+      expandedCategory === categoryName ? products : products.slice(0, 3);
+    return renderMenuItems(itemsToShow);
   };
 
   return (
@@ -68,7 +64,7 @@ const Menu = () => {
           searchTerm={searchTerm}
           renderMenuItems={renderMenuItems}
           filteredProducts={menu
-            .reduce((acc, { products }) => acc.concat(products), [])
+            .flatMap(({ products }) => products)
             .filter((product) =>
               product.productName
                 .toLowerCase()
@@ -79,25 +75,10 @@ const Menu = () => {
         <>
           <Box
             component="nav"
-            sx={{
-              padding: {
-                xs: "2rem 1rem",
-                sm: "2.4rem 6rem",
-                md: "2.4rem 13.2rem",
-              },
-              borderTop: ".3rem solid #1F3A33",
-              borderBottom: ".3rem solid #1F3A33",
-            }}
+            sx={styles.navContainer}
             aria-label="Menu categories"
           >
-            <List
-              sx={{
-                display: "flex",
-                flexDirection: "row",
-                justifyContent: "space-between",
-                padding: 0,
-              }}
-            >
+            <List sx={styles.navList}>
               {menuBar.map((item, index) => (
                 <MenuItem
                   key={index}
---
+++ webshop-frontend/src/components/Menu/styles.js
@@ -0,0 +1,23 @@
+const styles = {
+    scroll: {
+        behavior: "smooth",
+        block: "start",
+    },
+    navContainer: {
+        padding: {
+            xs: "2rem 1rem",
+            sm: "2.4rem 6rem",
+            md: "2.4rem 13.2rem",
+        },
+        borderTop: ".3rem solid #1F3A33",
+        borderBottom: ".3rem solid #1F3A33",
+    },
+    navList: {
+        display: "flex",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        padding: 0,
+    }
+};
+
+export default styles;
