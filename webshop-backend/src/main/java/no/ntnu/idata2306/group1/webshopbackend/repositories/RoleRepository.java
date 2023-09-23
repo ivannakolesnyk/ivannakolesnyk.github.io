7122368cf16490bc4041b8bca33df77d82a3d1b0
ivannakolesnyk <113033729+ivannakolesnyk@users.noreply.github.com>
2023-09-23 17:43:56 +0200

    Delete files

--- webshop-backend/src/main/java/no/ntnu/idata2306/group1/webshopbackend/repositories/RoleRepository.java
+++
@@ -1,8 +0,0 @@
-package no.ntnu.idata2306.group1.webshopbackend.repositories;
-
-import no.ntnu.idata2306.group1.webshopbackend.models.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface RoleRepository extends JpaRepository<Role, Long> {
-  Role findOneByName(String name);
-}
